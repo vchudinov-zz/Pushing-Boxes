97672ff87a63d84b2139f300eec595286caf3175
vchudinov <vchudinov@yahoo.com>
2013-05-13 19:07:18 +0200

    Update Localizable.java

--- src/PushingBoxes/Localizable.java
+++ src/PushingBoxes/Localizable.java
@@ -1,23 +1,15 @@
 package PushingBoxes;
 
-/** Localizable refers to all the objects of the game without the player.
- * It is further extended in the GameObject class.
- * PushBox - or the boxes the player pushes around are an implementation of Localizable.
- * The player could also be an implementation of this class, 
- * but we decided to keep him as a separate class, 
- * because the Localizable move method takes slightly different arguments. 
- * There is probably a way around this, but we couldn't find it
- * 
- * @see GameObject
- * @see PushBox
- * @see Player
- * @see Model
- * @version 3
- * 
- * @author Viktor and Nour
- * 
- 
-*/
+/** Deals with the movement of objects.
+     * In this version the only movable objects are the PushBoxes and the player. 
+          * 
+     * @param direction - the direction of movement
+     * @param model - model has all the necessary things for movement -  the gutter and the step size.
+     * @see Player
+     * @see Model
+     * 
+     * @version 5 
+     */
 public interface Localizable 
 {   /**The directions in which an object can move. 
     * Their names are self-explanatory */
